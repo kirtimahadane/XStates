21340e71a68544826b9eb44745442b6d0ea5937a
kirtimahadane <kirti.mahadane@gmail.com>
2024-01-02 10:20:58 +0530

    first  commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import CitySelector from "./CitySelector";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <CitySelector />
+    </div>
+  );
+}
